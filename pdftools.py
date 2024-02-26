149ac931d03b65d11de803c276148190c8ee95d4
lookmhen <lookmhen@gmail.com>
2024-02-26 22:55:35 +0700

    add WGSI server

--- pdftools.py
+++ pdftools.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request, send_file
 from PyPDF2 import PdfWriter, PdfReader
 from werkzeug.utils import secure_filename
+from waitress import serve
+import socket
+import uuid
+import os
+import time
+import logging
 
 app = Flask(__name__)
+tempdir=app.config['UPLOAD_FOLDER'] = 'temp'
+
+# Define the maximum lifetime of a temporary file in seconds
+MAX_LIFETIME = 120  # 2 min
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def index():
@@ -18,20 +32,30 @@ def process_pdf(processing_function):
         return "ไม่ได้เลือกไฟล์"
 
     if file:
-        filename = secure_filename(file.filename)
-        file.save(filename)
+        temp_filename = os.path.join(tempdir, str(uuid.uuid4()) + "-" + secure_filename(file.filename))
+        file.save(temp_filename)
 
-        reader = PdfReader(filename)
+        reader = PdfReader(temp_filename)
         writer = PdfWriter()
 
-        # Process the PDF using the provided processing_function
         processing_function(reader, writer)
 
-        output_filename = 'output-' + filename
+        output_filename = os.path.join(tempdir, 'output-' + secure_filename(file.filename))
         with open(output_filename, "wb") as fp:
             writer.write(fp)
 
-        return send_file(output_filename, as_attachment=True)
+        # Attempt to send the file to the client
+        try:
+            return send_file(output_filename, as_attachment=True)
+        finally:
+            try:
+                os.remove(temp_filename)
+                print(f"Temporary file '{temp_filename}' successfully removed.")
+            except Exception as e:
+                print(f"Error occurred while removing temporary file '{temp_filename}': {str(e)}")
+
+
+
 
 @app.route('/rotate', methods=['POST'])
 def rotate_pages():
@@ -51,6 +75,7 @@ def rotate_pages():
 
     return process_pdf(rotate_function)
 
+
 @app.route('/split', methods=['POST'])
 def split_pages():
     def split_function(reader, writer):
@@ -66,5 +91,47 @@ def split_pages():
 
     return process_pdf(split_function)
 
+
+def cleanup_temp_files():
+    
+    current_time = time.time()
+    
+    for file_name in os.listdir(tempdir):
+        file_path = os.path.join(tempdir, file_name)
+        file_age = current_time - os.path.getctime(file_path)
+
+        if file_age > MAX_LIFETIME:
+            try:
+                os.remove(file_path)
+                logger.info("Removed expired file: %s", file_path)
+            except Exception as e:
+                logger.error("Error removing file: %s", e)
+
+
+
+
+def ensure_temp_directory_exists():
+
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+        
+
+# Run the cleanup function periodically
+@app.before_request
+def run_cleanup():
+    cleanup_temp_files()
+
+
+def start_server():
+
+    host = socket.gethostbyname(socket.gethostname())
+    port = int(os.environ.get('PORT', 80))
+
+    ensure_temp_directory_exists()
+
+    logger.info("Starting server on %s:%d%s", host, port)
+
+    serve(app, host=host, port=port)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    start_server()
